1399ce1d8a51f7f0af587873c3c7915d4923fea4
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-16 21:53:46 +0100

    chore: only push

--- bin/create-branch.js
+++ bin/create-branch.js
@@ -2,13 +2,6 @@
 
 const Git = require('simple-git/promise');
 
-const gitUser = process.argv[2];
-const gitPassword = process.argv[3];
-const gitRepo = process.argv[4];
-const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`; 
-
-Git().addRemote('origin', repoUrl);
-
 Git().revparse(['--short', 'HEAD']).then((sha) => {
     return Git().checkoutBranch(`release/${sha}`, 'master')
         .then(Git().push('origin', `release/${sha}`))
