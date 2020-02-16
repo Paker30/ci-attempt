49ed44ebc8949432c8490e6552c52c5123e83e56
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-17 00:05:05 +0100

    fix(ci): pass arguments to rest of scripts

--- bin/follow-tags.js
+++ bin/follow-tags.js
@@ -5,8 +5,12 @@ const Git = require('simple-git/promise');
 const gitUser = process.argv[2];
 const gitPassword = process.argv[3];
 const gitRepo = process.argv[4];
-const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`; 
+const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`;
 
-Git().pushTags(repoUrl)
-    .then((status) => console.log('push tags', status))
-    .catch((error) => console.error('something went wrong', error));
+Git().revparse(['--abbrev-ref', 'HEAD']).then((currentBranch) =>
+    Promise.all([
+        Git().push(repoUrl, currentBranch)
+        , Git().pushTags(repoUrl)
+    ])
+        .then((status) => console.log('push tags', status))
+        .catch((error) => console.error('something went wrong', error));
