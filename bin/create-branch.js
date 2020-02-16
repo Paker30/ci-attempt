586a6ec1e05b58ce50d20a19686c85811c2afd9e
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-16 21:42:09 +0100

    fix(git): add parentheses

--- bin/create-branch.js
+++ bin/create-branch.js
@@ -7,7 +7,7 @@ const gitPassword = process.argv[3];
 const gitRepo = process.argv[4];
 const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`; 
 
-Git.addRemote('origin', repoUrl);
+Git().addRemote('origin', repoUrl);
 
 Git().revparse(['--short', 'HEAD']).then((sha) => {
     return Git().checkoutBranch(`release/${sha}`, 'master')
