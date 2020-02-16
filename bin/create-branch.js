adfb7d95d0dd4bfa1665051e49a8e58ca79858f0
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-17 00:05:05 +0100

    fix(ci): pass arguments to rest of scripts

--- bin/create-branch.js
+++ bin/create-branch.js
@@ -3,14 +3,9 @@
 const Git = require('simple-git/promise');
 
 const gitUser = process.argv[2];
-console.log('user', process.argv[2]);
 const gitPassword = process.argv[3];
-console.log('pass', process.argv[3]);
 const gitRepo = process.argv[4];
-console.log('repo', process.argv[4]);
 const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`; 
-
-console.log('git repo', repoUrl);
 
 Git().revparse(['--short', 'HEAD']).then((sha) => {
     return Git().checkoutBranch(`release/${sha}`, 'master')
