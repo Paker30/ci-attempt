c0468ca302687fe681e55e646e6c78bf85d4a289
Francisco Moreno <fmoreno@MacBook-Pro-de-Yudonpay.local>
2020-02-20 16:58:09 +0100

    fix: undo process.exit

--- bin/create-branch.js
+++ bin/create-branch.js
@@ -6,11 +6,10 @@ const gitUser = process.argv[2];
 const gitPassword = process.argv[3];
 const gitRepo = process.argv[4];
 const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`;
-console.log('repo', repoUrl);
 
 Git().revparse(['--short', 'HEAD']).then((sha) => {
     return Git().checkoutBranch(`release/${sha}`, 'master')
         .then(Git().push(repoUrl, `release/${sha}`, ['--set-upstream']))
         .then(() => console.log(`create branch release/${sha}`))
     })
-    .catch((error) => console.error('something went wrong', error), process.exit(1));
+    .catch((error) => console.error('something went wrong', error));
