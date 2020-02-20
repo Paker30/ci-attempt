970395e341bbbeb92d1e05cdd22792e9216a1b8a
Francisco Moreno <fmoreno@MacBook-Pro-de-Yudonpay.local>
2020-02-20 16:04:20 +0100

    fix(pr): pr has to be done through github api

--- bin/create-PR.js
+++ bin/create-PR.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 const Git = require('simple-git/promise');
+const Axios = require('axios');
 
 const gitUser = process.argv[2];
-const gitPassword = process.argv[3];
-const gitRepo = process.argv[4];
-const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`;
+const gitRepo = process.argv[3];
 
 Git().revparse(['--abbrev-ref', 'HEAD'])
-    .then((currentBranch) => Git().raw(['request-pull', '-p', 'master', './']))
-    .then((status) => console.log('create PR', status))
-    .catch((error) => console.error('something went wrong', error), process.exit(1));
+    .then((currentBranch) =>
+        Axios.post(`https://api.github.com/repos/${gitUser}/${gitRepo}/pulls`, {
+            title: 'chore: bump version [skip ci]',
+            body: 'this is a release PR, check version and changelog have been updated',
+            base: 'master',
+            head: currentBranch
+        }))
+    .then((status) => console.log('pull request created', status))
+    .catch((error) => console.error('something went wrong creating the pull request', error), process.exit(1));
