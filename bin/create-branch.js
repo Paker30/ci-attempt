636c9e207ef3dba4530f46ee989bda9709aa046b
Francisco Moreno <fmoreno@MacBook-Pro-de-Yudonpay.local>
2020-02-24 13:44:46 +0100

    feat(branch): create ref by api

--- bin/create-branch.js
+++ bin/create-branch.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 const Git = require('simple-git/promise');
+const Axios = require('axios');
 
 Git().revparse(['--short', 'HEAD']).then((sha) => {
-    return Git().checkoutBranch(`release/${sha}`, 'master')
-        .then(() => console.log(`create branch release/${sha}`))
+    return Axios.post(`https://api.github.com/repos/${gitUser}/${gitRepo}/refs`, {
+        ref: `refs/heads/release${sha}`,
+        sha: sha
+    }, {
+        headers: { 'Authorization': `token ${gitToken}` }
     })
+        .then(() => console.log(`create branch release/${sha}`))
+})
     .catch((error) => console.error('something went wrong', error));
