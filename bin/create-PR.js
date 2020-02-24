06e9951a5b303dcc244611f7d7381a6ac14e787e
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-24 22:12:49 +0100

    fix: missing /

--- bin/create-PR.js
+++ bin/create-PR.js
@@ -9,7 +9,7 @@ const gitToken = process.argv[4];
 
 Git().revparse(['HEAD']).then((sha) => {
     const shortSha = sha.slice(0, 7);
-    return Axios.put(`https://api.github.com/repos/${gitUser}/${gitRepo}/git/refs/release${shortSha}`, {
+    return Axios.put(`https://api.github.com/repos/${gitUser}/${gitRepo}/git/refs/origin/try_again`, {
         sha: sha
     }, {
         headers: { 'Authorization': `token ${gitToken}` }
