7f5abc92a73f71a6fd16d9ff375d00656cc1c518
Francisco Moreno Cantero <fmoreno@marcoaldany.com>
2020-02-24 19:43:04 +0100

    chore(push): without release

--- bin/create-PR.js
+++ bin/create-PR.js
@@ -8,7 +8,7 @@ const gitRepo = process.argv[3];
 const gitToken = process.argv[4];
 
 Git().revparse(['HEAD']).then((sha) =>
-    Axios.put(`https://api.github.com/repos/${gitUser}/${gitRepo}/git/refs/release/${sha.slice(0,7)}}`, {
+    Axios.put(`https://api.github.com/repos/${gitUser}/${gitRepo}/git/refs/${sha.slice(0,7)}}`, {
         sha: sha
     }, {
         headers: { 'Authorization': `token ${gitToken}` }
