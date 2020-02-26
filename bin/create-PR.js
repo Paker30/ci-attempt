6eca0657506493441ccb0ff3946943a1f0da4ced
Francisco Moreno <fmoreno@MacBook-Pro-de-Yudonpay.local>
2020-02-26 09:32:09 +0100

    feat(token): change git uris to work with token

--- bin/create-PR.js
+++ bin/create-PR.js
@@ -4,7 +4,8 @@ const Git = require('simple-git/promise');
 const Axios = require('axios');
 
 const gitUser = process.argv[2];
-const gitRepo = process.argv[3];
+const gitToken = process.argv[3];
+const gitRepo = process.argv[4];
 
 Git().revparse(['--abbrev-ref', 'HEAD'])
     .then((currentBranch) =>
@@ -13,6 +14,8 @@ Git().revparse(['--abbrev-ref', 'HEAD'])
             body: 'this is a release PR, check version and changelog have been updated',
             base: 'master',
             head: currentBranch
+        }, {
+            headers: { 'Authorization': `token ${gitToken}`, 'Content-Type': 'application/json' }
         }))
     .then((status) => console.log('pull request created', status))
-    .catch((error) => console.error('something went wrong creating the pull request', error);
+    .catch((error) => console.error('something went wrong creating the pull request', error));
--- bin/follow-tags.js
+++ bin/follow-tags.js
@@ -3,9 +3,9 @@
 const Git = require('simple-git/promise');
 
 const gitUser = process.argv[2];
-const gitPassword = process.argv[3];
+const gitToken = process.argv[3];
 const gitRepo = process.argv[4];
-const repoUrl = `https://${gitUser}:${gitPassword}@github.com/${gitUser}/${gitRepo}`;
+const repoUrl = `https://${gitToken}@github.com/${gitUser}/${gitRepo}.git`;
 
 Git().revparse(['--abbrev-ref', 'HEAD'])
     .then((currentBranch) => Promise.all([
